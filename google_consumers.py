b28bbfe44adfe23041409631dd98cd105e726243
alexruzzi98 <42788529+alexruzzi98@users.noreply.github.com>
2018-09-23 01:43:58 +0200

    Update google_consumers.py

--- google_consumers.py
+++ google_consumers.py
@@ -5,44 +5,56 @@ try:
     import os
     from pandas.io.json import json_normalize
     from collections import Counter
-    from sklearn.preprocessing import LabelEncoder
     import datetime
+    import lightgbm as lgb
+    from sklearn import preprocessing
 except:
     print('error')
 
 
-def load_df(csv_path='../input/train.csv', nrows=None):
+def load_df(csv_path='train.csv', nrows=None):
     JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
-
+    print(1)
     df = pd.read_csv(csv_path,
                      converters={column: json.loads for column in JSON_COLUMNS},
                      dtype={'fullVisitorId': 'str'},  # Important!!
                      nrows=nrows)
-
+    print(2)
     for column in JSON_COLUMNS:
+        print(3)
         column_as_df = json_normalize(df[column])
         column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
         df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
     print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
     return df
 
-df = load_df(csv_path='train.csv',nrows=20000)
-df["totals.transactionRevenue"] =df["totals.transactionRevenue"].astype('float')
-gdf = df.groupby("fullVisitorId")["totals.transactionRevenue"].sum().reset_index()
-nzi = pd.notnull(df["totals.transactionRevenue"]).sum()
+train_df = load_df(nrows=10000)
+test_df = load_df("test.csv")
+
+train_df['date'] = pd.to_datetime(train_df['date'].apply(lambda x: str(x)[:4] + '-' + str(x)[4:6] + '-' + str(x)[6:]))
+test_df['date'] = pd.to_datetime(test_df['date'].apply(lambda x: str(x)[:4] + '-' + str(x)[4:6] + '-' + str(x)[6:]))
+train_df["totals.transactionRevenue"] = train_df["totals.transactionRevenue"].astype('float')
+gdf = train_df.groupby("fullVisitorId")["totals.transactionRevenue"].sum().reset_index()
+nzi = pd.notnull(train_df["totals.transactionRevenue"]).sum()
 nzr = (gdf["totals.transactionRevenue"]>0).sum()
-print("Number of instances in train set with non-zero revenue : ", nzi, " and ratio is : ", nzi /df.shape[0])
+print("Number of instances in train set with non-zero revenue : ", nzi, " and ratio is : ", nzi / train_df.shape[0])
 print("Number of unique customers with non-zero revenue : ", nzr, "and the ratio is : ", nzr / gdf.shape[0])
 
-for col in ['visitNumber', 'totals.hits', 'totals.pageviews', 'totals.transactionRevenue']:
-    df[col] = df[col].astype(float)
+'''
+for col in train_df:
+    print(col)
+for col in train_df:
+    print(40 * '-')
+    print(col)
 
-df['totals.transactionRevenue'].fillna(0,inplace = True)
+    count =0
+    for row in train_df[col]:
 
+            print(row)
 
+'''
 
-
-df.drop(['socialEngagementType',
+cols_to_drop = ['socialEngagementType',
  'device.browserSize',
  'device.browserVersion',
  'device.flashVersion',
@@ -59,16 +71,102 @@ df.drop(['socialEngagementType',
  'geoNetwork.latitude',
  'geoNetwork.longitude',
  'geoNetwork.networkLocation',
- 'totals.bounces',
- 'totals.newVisits',
- 'totals.visits',
  'trafficSource.adwordsClickInfo.criteriaParameters',
  'trafficSource.adwordsClickInfo.isVideoAd',
- 'trafficSource.isTrueDirect',
- 'sessionId']
-,1,inplace= True)
+ 'sessionId',"geoNetwork.subContinent","geoNetwork.continent", "device.browser", "trafficSource.adwordsClickInfo.slot",
+ "trafficSource.adwordsClickInfo.adNetworkType","trafficSource.adwordsClickInfo.page"]
 
-for column in df:
-    print(column)
-    
-    
+train_df = train_df.drop(cols_to_drop, axis=1)
+test_df = test_df.drop(cols_to_drop, axis=1)
+
+# Impute 0 for missing target values
+train_df["totals.transactionRevenue"].fillna(0, inplace=True)
+train_y = train_df["totals.transactionRevenue"].values
+train_id = train_df["fullVisitorId"].values
+test_id = test_df["fullVisitorId"].values
+
+
+# label encode the categorical variables and convert the numerical variables to float
+cat_cols = ["channelGrouping", "device.deviceCategory", "device.operatingSystem",
+            "geoNetwork.city",  "geoNetwork.country", "geoNetwork.metro",
+            "geoNetwork.networkDomain", "geoNetwork.region", "trafficSource.adContent",
+            "trafficSource.adwordsClickInfo.gclId",
+            "trafficSource.campaign",
+            "trafficSource.keyword", "trafficSource.medium", "trafficSource.referralPath", "trafficSource.source",
+            'trafficSource.isTrueDirect','totals.newVisits'
+            ]
+for col in cat_cols:
+    print(col)
+    lbl = preprocessing.LabelEncoder()
+    lbl.fit(list(train_df[col].values.astype('str')) + list(test_df[col].values.astype('str')))
+    train_df[col] = lbl.transform(list(train_df[col].values.astype('str')))
+    test_df[col] = lbl.transform(list(test_df[col].values.astype('str')))
+
+
+num_cols = ["totals.hits", "totals.pageviews", "visitNumber", "visitStartTime"]
+for col in num_cols:
+    train_df[col] = train_df[col].astype(float)
+    test_df[col] = test_df[col].astype(float)
+
+# Split the train dataset into development and valid based on time
+dev_df = train_df[train_df['date']<=pd.Timestamp(2017,5,31)]
+val_df = train_df[train_df['date']>pd.Timestamp(2017,5,31)]
+dev_y = np.log1p(dev_df["totals.transactionRevenue"].values)
+val_y = np.log1p(val_df["totals.transactionRevenue"].values)
+
+dev_X = dev_df[cat_cols + num_cols]
+val_X = val_df[cat_cols + num_cols]
+test_X = test_df[cat_cols + num_cols]
+
+# custom function to run light gbm model
+def run_lgb(train_X, train_y, val_X, val_y, test_X):
+    params = {
+        "objective": "regression",
+        "metric": "rmse",
+        "num_leaves": 30,
+        "min_child_samples": 100,
+        "learning_rate": 0.1,
+        "bagging_fraction": 0.7,
+        "feature_fraction": 0.5,
+        "bagging_frequency": 5,
+        "bagging_seed": 2018,
+        "verbosity": -1
+    }
+
+
+    lgtrain = lgb.Dataset(train_X, label=train_y)
+    lgval = lgb.Dataset(val_X, label=val_y)
+    model = lgb.train(params, lgtrain, 1000, valid_sets=[lgval], early_stopping_rounds=100, verbose_eval=100)
+
+    pred_test_y = model.predict(test_X, num_iteration=model.best_iteration)
+    return pred_test_y, model
+
+# Training the model #
+pred_test, model = run_lgb(dev_X, dev_y, val_X, val_y, test_X)
+
+
+
+'''
+print( 100 * 'X')
+
+for col in dev_X:
+    print(40 * '-')
+    print(col)
+    count =0
+    for row in dev_X[col]:
+        print(row)
+        count +=1
+        if count >20:
+            break
+'''
+
+
+sub_df = pd.DataFrame({"fullVisitorId":test_id})
+pred_test[pred_test<0] = 0
+sub_df["PredictedLogRevenue"] = np.expm1(pred_test)
+sub_df = sub_df.groupby("fullVisitorId")["PredictedLogRevenue"].sum().reset_index()
+sub_df.columns = ["fullVisitorId", "PredictedLogRevenue"]
+sub_df["PredictedLogRevenue"] = np.log1p(sub_df["PredictedLogRevenue"])
+sub_df.to_csv("tosend.csv", index=False)
+
+print(sub_df.head())
